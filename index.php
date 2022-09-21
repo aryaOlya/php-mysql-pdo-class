5706128443d7d5621f45c96b28b8a3886ac4cfb9
arya olya <99118866+aryaOlya@users.noreply.github.com>
2022-09-22 00:06:05 +0330

    WIP

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 class usePdo{
-
     public $server;
     public $username;
     public $password;
@@ -25,11 +24,33 @@ class usePdo{
 
     //insert data to the $tableName table columns
     public function insertToTable($tableName,$columns,$values){
-        $statement = $this->conn->prepare("insert into"." ".$tableName." "."(".implode(',',$columns).")"." "."values"."(".$this->setBindValues($values).")");
+        $statement = $this->conn->prepare("insert into"." ".$tableName." "."(".$this->arrayToString($columns).")"." "."values"."(".$this->setBindValues($values).")");
         $statement->execute($values);
     }
 
 
+    //check the table existence
+    public function __invoke($tableName)
+    {
+        try {
+            $statement = $this->conn->prepare("select * from ".$tableName);
+            $statement->execute();
+            $assoc = $statement->fetch(PDO::FETCH_ASSOC);
+            $tableInfo = '';
+            foreach ($assoc as $key => $value){
+                $tableInfo =$key."=>".$value." | ".$tableInfo."<br />";
+            }
+            return $tableInfo;
+        }catch(PDOException $e){
+            return "no such table exist!";
+        }
+    }
+
+
+    //convert array to string
+    private function arrayToString($columns){
+        return implode(',',$columns);
+    }
 
     //convert values to ?
     private function setBindValues($values){
@@ -39,14 +60,27 @@ class usePdo{
         return implode(',',$values);
     }
 
-    //execute when method not found
+    //execute when dynamic method not found
     public function __call(string $name, array $arguments)
     {
-        return 'method '.$name.' not found!' ;
+        echo 'dynamic method '.$name.' not found!' ;
+    }
+
+    //execute when static method not found
+    public static function __callStatic(string $name, array $arguments)
+    {
+        echo 'static method '.$name.' not found!' ;
+    }
+
+    //execute when instance got printed
+    public function __toString(): string
+    {
+        return 'try to connect to the '.$this->dbName.' database!';
     }
 }
 
 $myPdo = new UsePdo('localhost','root','','testone');
-$myPdo->insertToTable('user',['name'],['asghar']);
+echo $myPdo('user');
+//$myPdo->insertToTable('user',['name'],['ahmad']);
 
 
